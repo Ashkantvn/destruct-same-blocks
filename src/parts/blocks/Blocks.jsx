86a9c7c9ac3a56a6a1a7985dba5a95a5bc9a64b7
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-28 16:57:26 +0330

    Random color for each blocks

--- src/parts/blocks/Blocks.jsx
+++ src/parts/blocks/Blocks.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const Blocks = () => {
   const [BlocksData, setBlocksData] = useState([]);
+  const colors = ["#002984","#ba000d","#006400","#e1d9d1"];
 
   ///set row and column
   useEffect(() => {
@@ -18,9 +19,12 @@ const Blocks = () => {
     };
   }, []);
 
+  const randomNumber = ()=>{
+    return Math.floor(Math.random()*4);
+  }
   ///mapping blocks data
   const mappedBlocks = BlocksData.map((item) => (
-    <div className="block" key={item.id}></div>
+    <div className="block" style={{backgroundColor:colors[randomNumber()]}} key={item.id}></div>
   ));
 
   return <div className="blocks-div">{mappedBlocks}</div>;
