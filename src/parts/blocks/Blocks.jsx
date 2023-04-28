0084cc9cf17985f7c5f554ca3f41d9e7e752204c
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-28 23:24:50 +0330

    Update click handler function (same block destructure)

--- src/parts/blocks/Blocks.jsx
+++ src/parts/blocks/Blocks.jsx
@@ -4,16 +4,20 @@ const Blocks = () => {
   const [BlocksData, setBlocksData] = useState([]);
   const colors = ["#002984", "#ba000d", "#006400", "#e1d9d1"];
   const [finishedGame, setfinishedGame] = useState(false);
+  const [score, setscore] = useState(0);
+  const targetColor = useRef("");
   const blocksRef = useRef([]);
   const count = useRef(1);
+  const time = useRef(3);
+  const currentScore = useRef(0);
   const randomNumber = () => {
     return Math.floor(Math.random() * 4);
   };
 
   ///set row and column
   useEffect(() => {
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 10; j++) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
         setBlocksData((BlocksData) => [
           ...BlocksData,
           { row: i, column: j, id: `${i}${j}` },
@@ -26,17 +30,35 @@ const Blocks = () => {
   }, []);
 
   const gameOverChecker = () => {
-    if(count.current>80)  setfinishedGame(true);
-    count.current +=1;
+    if (count.current > 81) {
+      setfinishedGame(true);
+      setscore(currentScore.current);
+    }
     console.log(count.current);
   };
   ///click handler
   const sameBlocksDestructure = (clickEvent) => {
-    if(clickEvent.target.style.backgroundColor === "black") return;
-    clickEvent.target.style.backgroundColor = "black";
+    if (clickEvent.target.style.backgroundColor === "black") return;
+    if (!targetColor.current) {
+      targetColor.current = clickEvent.target.style.backgroundColor;
+      const interval = setInterval(() => {
+        time.current -= 1;
+        if (time.current === 0) {
+          time.current = 3;
+          alert("select new color");
+          targetColor.current = "";
+          currentScore.current -= 10;
+          clearInterval(interval);
+        }
+      }, 1000);
+    }
+    if(targetColor.current === clickEvent.target.style.backgroundColor){
+      clickEvent.target.style.backgroundColor = "black";
+      currentScore.current +=1;
+      count.current += 1;
+    }
     gameOverChecker();
-  };
-
+    };
   ///mapping blocks data
   const mappedBlocks = BlocksData.map((item, i) => (
     <div
@@ -50,7 +72,11 @@ const Blocks = () => {
   return (
     <div>
       {finishedGame ? (
-        <div className="game-over">Game over</div>
+        <div className="game-over">
+          Game over
+          <h2>Score : {score}</h2>
+          <button onClick={nextRound}>Next Round</button>
+        </div>
       ) : (
         <div className="blocks-div">{mappedBlocks}</div>
       )}
