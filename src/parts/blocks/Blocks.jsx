8abf966d6dfc988f1d584bbd9fd7debe106829dd
AshkanTavangar <ashkantavangar7@gmail.com>
2023-04-28 19:17:39 +0330

    Add initial destruction(click event) function

--- src/parts/blocks/Blocks.jsx
+++ src/parts/blocks/Blocks.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Blocks = () => {
   const [BlocksData, setBlocksData] = useState([]);
-  const colors = ["#002984","#ba000d","#006400","#e1d9d1"];
+  const colors = ["#002984", "#ba000d", "#006400", "#e1d9d1"];
+  const blocksRef = useRef([]);
 
   ///set row and column
   useEffect(() => {
@@ -10,7 +11,7 @@ const Blocks = () => {
       for (let j = 0; j < 9; j++) {
         setBlocksData((BlocksData) => [
           ...BlocksData,
-          { BlocksData: i, column: j, id: `${i}${j}` },
+          { row: i, column: j, id: `${i}${j}` },
         ]);
       }
     }
@@ -19,12 +20,53 @@ const Blocks = () => {
     };
   }, []);
 
-  const randomNumber = ()=>{
-    return Math.floor(Math.random()*4);
-  }
+  const randomNumber = () => {
+    return Math.floor(Math.random() * 4);
+  };
+
+  //get Element row and column
+  const getBlockData = (block) => {
+    const selectedRow = Number(block.id.slice(0, 1));
+    const selectedColumn = Number(block.id.slice(2, 3));
+    const selectedColor = block.style.backgroundColor;
+    return { selectedRow, selectedColumn, selectedColor };
+  };
+
+  ///click handler
+  const sameBlocksDestructure = (event) => {
+    const clickedBlockData = getBlockData(event.target);
+    const conditions = [
+      `${clickedBlockData.selectedRow}-${clickedBlockData.selectedColumn + 1}`,
+      `${clickedBlockData.selectedRow}-${clickedBlockData.selectedColumn - 1}`,
+      `${clickedBlockData.selectedRow - 1}-${clickedBlockData.selectedColumn}`,
+      `${clickedBlockData.selectedRow + 1}-${clickedBlockData.selectedColumn}`,
+    ];
+    const filteredBlocks = blocksRef.current.filter((item) => {
+      return (
+        item.id.includes(conditions[0]) ||
+        item.id.includes(conditions[1]) ||
+        item.id.includes(conditions[2]) ||
+        item.id.includes(conditions[3])
+      );
+    });
+     filteredBlocks.forEach((item) => {
+      if(!(item.style.backgroundColor == clickedBlockData.selectedColor)) return ;
+      event.target.style.display= "none";
+      item.style.display = "none";
+      console.log(item);
+    })
+  };
+
   ///mapping blocks data
-  const mappedBlocks = BlocksData.map((item) => (
-    <div className="block" style={{backgroundColor:colors[randomNumber()]}} key={item.id}></div>
+  const mappedBlocks = BlocksData.map((item, i) => (
+    <div
+      onClick={sameBlocksDestructure}
+      ref={(element) => (blocksRef.current[i] = element)}
+      className="block"
+      style={{ backgroundColor: colors[randomNumber()] }}
+      key={item.id}
+      id={`${item.row}-${item.column}`}
+    ></div>
   ));
 
   return <div className="blocks-div">{mappedBlocks}</div>;
